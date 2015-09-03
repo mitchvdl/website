b141883dd951d9639b0dbcd40ab43446909500d5
Mitch Vanderlinden <m.vanderlinden@geschenkidee.de>
2015-09-03 14:58:18 +0200

    more updates

--- index.php
+++ index.php
@@ -4,26 +4,28 @@ define('DS', DIRECTORY_SEPARATOR);
 
 require 'vendor' . DS . 'autoload.php';
 
+// Required bootstraps
 $templates = new League\Plates\Engine('web' . DS . 'templates', 'phtml');
+$templates->loadExtension(new League\Plates\Extension\URI($_SERVER['REQUEST_URI']));
 $templates->addFolder('gems', 'web' . DS . 'templates' . DS . 'gems', 'phtml');
 $templates->addFolder('body', 'web' . DS . 'templates' . DS . 'body', 'phtml');
 $templates->addFolder('modules', 'web' . DS . 'templates' . DS . 'modules', 'phtml');
 
-echo $templates->render('layout/header');
-
-echo $templates->render('gems::header');
-
-echo $templates->render('gems::body_start');
-
-
-// Very simple file_exist logic.
-
-
-$parts = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+// 'router' logic
+$parts = explode('/', strtolower(trim($_SERVER['REQUEST_URI'], '/')));
 
 $file = strtolower($parts[0]);
 
-switch ( $file )
+// basic pages
+
+
+echo $templates->render('layout/header');
+if ( !isset($parts[1]) || 'preview' != $parts[1]) {
+    echo $templates->render('gems::header');
+}
+echo $templates->render('gems::body_start');
+
+switch ( (isset($parts[0]) ? $parts[0] : 'home') )
 {
     case 'katai-css-js-merge-version':
         echo $templates->render('modules::' . $file);
@@ -34,8 +36,7 @@ switch ( $file )
         break;
 
     case 'cv' :
-
-        switch ( (isset($parts[1]) ? strtolower($parts[1]) : 'home') ) {
+        switch ( (isset($parts[1]) ? $parts[1] : 'home') ) {
             case 'work-experience':
                 echo $templates->render('body::cv/workexperience');
                 break;
@@ -52,5 +53,7 @@ switch ( $file )
 
 
 echo $templates->render('gems::body_end');
-
+if ( !isset($parts[1]) || 'preview' != $parts[1]) {
+    echo $templates->render('gems::footer');
+}
 echo $templates->render('layout/footer');
